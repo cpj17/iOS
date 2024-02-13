8edc76c3c5c5e450e39fb8712fad87ff3381ec16
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-13 17:26:32 +0530

    Add files via upload

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ZXing.Net.Mobile.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarcodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string stringbarcode = "";
+
+                var sc = new ZXingScannerPage();
+                await Navigation.PushAsync(sc);
+                sc.OnScanResult += (result) =>
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Navigation.PopAsync();
+                        stringbarcode = result.Text;
+                        
+                    });
+
+                };
+            }
+            catch (Exception objException)
+            {
+                await DisplayAlert("Exception", "btnScanner_Clicked : " + objException, "OK");
+                objException = null;
+            }
+        }
+    }
+}
